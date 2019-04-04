3f61a6800578ceb68c24ce77920c77de0c9e26e9
Spartak Bugdaryan <bugdaryan@gmail.com>
2019-04-05 00:25:45 +0400

    Add Asset detail model Update css style file Add Detail action on catalog controller Add Detail view

--- Library/Controllers/CatalogController.cs
+++ Library/Controllers/CatalogController.cs
@@ -35,5 +35,25 @@ namespace Library.Controllers
 
             return View(model);
         }
+
+        public IActionResult Detail(int id)
+        {
+            var asset = _assets.GetById(id);
+            var model = new AssetDetailModel
+            {
+                AssetId = id,
+                Title = asset.Title,
+                Year = asset.Year,
+                Cost = asset.Cost,
+                Status = asset.Status.Name,
+                ImageUrl = asset.ImageUrl,
+                AuthorOrDirector = _assets.GetAuthorOrDirector(asset.Id),
+                CurrentLocation = _assets.GetCurrentLocation(asset.Id).Name,
+                DeweyCallNumber = _assets.GetDeweyIndex(asset.Id),
+                ISBN = _assets.GetIsbn(asset.Id)
+            };
+
+            return View(model);
+        }
     }
 }
