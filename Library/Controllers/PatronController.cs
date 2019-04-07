806c40757bea27b1780388837c184210f34ff41e
Spartak Bugdaryan <bugdaryan@gmail.com>
2019-04-08 02:14:32 +0400

    Add patron controller with index action Fix add fees property to patron detail model

---
+++ Library/Controllers/PatronController.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using Library.Models.Patron;
+using LibraryData;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Controllers
+{
+    public class PatronController : Controller
+    {
+        private readonly IPatron _patron;
+
+        public PatronController(IPatron patron)
+        {
+            _patron = patron;
+        }
+
+        public IActionResult Index()
+        {
+            var allPatrons = _patron.GetAll();
+            var patronModels = allPatrons.Select(p => new PatronDetailModel
+            {
+                Id = p.Id,
+                FirstName = p.FirstName,
+                LastName = p.LastName,
+                CheckoutHistory = _patron.GetCheckoutHistory(p.Id),
+                HomeLibraryBranch = p.HomeLibraryBranch.Name,
+                AssetsCheckedOut = _patron.GetCheckouts(p.Id),
+                OverdueFees = p.LibraryCard.Fees,
+                Holds = _patron.GetHolds(p.Id),
+                LibraryCardId = p.LibraryCard.Id,
+                MemberSince = p.LibraryCard.Created,
+                Telephone = p.TelephoneNumber
+            }).ToList();
+
+            var model = new PatronIndexModel
+            {
+                Patrons = patronModels
+            };
+            return View(model);
+        }
+    }
+}
