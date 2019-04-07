5d941318b0b502d9a021ee97cc45dbfe3a835ebe
Spartak Bugdaryan <bugdaryan@gmail.com>
2019-04-08 02:46:26 +0400

    Add action and view for detail patron

--- Library/Controllers/PatronController.cs
+++ Library/Controllers/PatronController.cs
@@ -1,6 +1,8 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Library.Models.Patron;
 using LibraryData;
+using LibraryData.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Library.Controllers
@@ -36,6 +38,27 @@ namespace Library.Controllers
             {
                 Patrons = patronModels
             };
+            return View(model);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var p = _patron.Get(id);
+            var model = new PatronDetailModel
+            {
+                Id = p.Id,
+                FirstName = p.FirstName,
+                LastName = p.LastName,
+                CheckoutHistory = _patron.GetCheckoutHistory(p.Id),
+                HomeLibraryBranch = p.HomeLibraryBranch.Name,
+                AssetsCheckedOut = _patron.GetCheckouts(p.Id).ToList() ?? new List<Checkout>(),
+                OverdueFees = p.LibraryCard.Fees,
+                Holds = _patron.GetHolds(p.Id),
+                LibraryCardId = p.LibraryCard.Id,
+                MemberSince = p.LibraryCard.Created,
+                Telephone = p.TelephoneNumber
+            };
+
             return View(model);
         }
     }
