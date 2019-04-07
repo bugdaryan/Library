c72bbd1b8488d69a727304d1b86ff5fd47e7f88a
Spartak Bugdaryan <bugdaryan@gmail.com>
2019-04-08 02:02:24 +0400

    Add model for patron detail model and for patrons collection

---
+++ Library/Models/Patron/PatronDetailModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryData.Models;
+
+namespace Library.Models.Patron
+{
+    public class PatronDetailModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => FirstName + " " + LastName;
+        public int LibraryCardId { get; set; }
+        public DateTime MemberSince { get; set; }
+        public string Telephone { get; set; }
+        public string HomeLibraryBranch { get; set; }
+        public IEnumerable<LibraryData.Models.Checkout> AssetsCheckedOut { get; set; }
+        public IEnumerable<CheckoutHistory> CheckoutHistory { get; set; }
+        public IEnumerable<Hold> Holds { get; set; }
+    }
+}
