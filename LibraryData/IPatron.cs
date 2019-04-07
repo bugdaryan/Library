0295d00f3c3ae70facad31a31a0aeb2e6fdddd4e
Spartak Bugdaryan <bugdaryan@gmail.com>
2019-04-08 01:37:40 +0400

    Add patron service interface and interface implementation

---
+++ LibraryData/IPatron.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using LibraryData.Models;
+
+namespace LibraryData
+{
+    public interface IPatron
+    {
+        Patron Get(int id);
+        IEnumerable<Patron> GetAll();
+        void Add(Patron newPatron);
+
+        IEnumerable<CheckoutHistory> GetCheckoutHistory(int patronId);
+        IEnumerable<Hold> GetHolds(int patronId);
+        IEnumerable<Checkout> GetCheckouts(int patronId);
+    }
+}
---
+++ LibraryServices/PatronService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LibraryData;
+using LibraryData.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryServices
+{
+    public class PatronService : IPatron
+    {
+        private readonly LibraryContext _context;
+
+        public PatronService(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        public Patron Get(int id)
+        {
+            return GetAll()
+                .FirstOrDefault(p => p.Id == id);
+        }
+
+        public IEnumerable<Patron> GetAll()
+        {
+            return _context.Patrons
+                .AsNoTracking()
+                .Include(p => p.LibraryCard)
+                .Include(p => p.HomeLibraryBranch);
+        }
+
+        public void Add(Patron newPatron)
+        {
+            _context.Add(newPatron);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<CheckoutHistory> GetCheckoutHistory(int patronId)
+        {
+            var cardId = Get(patronId).LibraryCard.Id;
+            return _context.CheckoutHistories
+                .AsNoTracking()
+                .Include(h => h.LibraryCard)
+                .Include(h => h.LibraryAsset)
+                .Where(h => h.LibraryCard.Id == cardId)
+                .OrderByDescending(h => h.CheckedOut);
+        }
+
+        public IEnumerable<Hold> GetHolds(int patronId)
+        {
+            var cardId = Get(patronId).LibraryCard.Id;
+            return _context.Holds
+                .AsNoTracking()
+                .Include(h => h.LibraryCard)
+                .Include(h => h.LibraryAsset)
+                .Where(h => h.LibraryCard.Id == cardId)
+                .OrderByDescending(h=>h.HoldPlaced);
+        }
+
+        public IEnumerable<Checkout> GetCheckouts(int patronId)
+        {
+            var cardId = Get(patronId).LibraryCard.Id;
+            return _context.Checkouts
+                .AsNoTracking()
+                .Include(h => h.LibraryCard)
+                .Include(h => h.LibraryAsset)
+                .Where(h => h.LibraryCard.Id == cardId);
+        }
+    }
+}
