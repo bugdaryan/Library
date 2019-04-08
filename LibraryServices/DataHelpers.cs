4886ccead22bfa7b0a392c31c1ab4938c79ce990
Spartak Bugdaryan <bugdaryan@gmail.com>
2019-04-09 02:19:30 +0400

    Add library branch service implementation Add helper class for data manipulation

---
+++ LibraryServices/DataHelpers.cs
@@ -0,0 +1,37 @@
+﻿using LibraryData.Models;
+using System;
+using System.Collections.Generic;
+
+namespace LibraryServices
+{
+    public static class DataHelpers
+    {
+        public static List<string> HumanizeBizHours(IEnumerable<BranchHours> branchHours)
+        {
+            var hours = new List<string>();
+
+            foreach (var time in branchHours)
+            {
+                var day = HumanizeDay(time.DayOfWeek);
+                var openTime = HumanizeTime(time.OpenTime);
+                var closeTime = HumanizeTime(time.CloseTime);
+
+                var timeEntry = $"{day} {openTime} to {closeTime}";
+
+                hours.Add(timeEntry);
+            }
+
+            return hours;
+        }
+
+        public static string HumanizeTime(int time)
+        {
+            return TimeSpan.FromHours(time).ToString("hh':'mm");
+        }
+
+        public static string HumanizeDay(int dayOfWeek)
+        {
+            return Enum.GetName(typeof(DayOfWeek), dayOfWeek);
+        }
+    }
+}
