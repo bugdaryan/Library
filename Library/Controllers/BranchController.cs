016e7d688e55a4dab9c4fe9633f8473feb14fd3d
Spartak Bugdaryan <bugdaryan@gmail.com>
2019-04-09 02:26:31 +0400

    Add empty branch controller with library branch service

---
+++ Library/Controllers/BranchController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryData;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Controllers
+{
+    public class BranchController : Controller
+    {
+        private readonly ILibraryBranch _branch;
+        public BranchController(ILibraryBranch branch)
+        {
+            _branch = branch;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
