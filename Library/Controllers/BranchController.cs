bb0c884f327c0cff9c7770c2f3cc4e0b0dcb67a2
Spartak Bugdaryan <bugdaryan@gmail.com>
2019-04-10 20:44:27 +0400

    Add index and detail actions in branch controller

--- Library/Controllers/BranchController.cs
+++ Library/Controllers/BranchController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Library.Models.Branch;
 using LibraryData;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,7 +17,48 @@ namespace Library.Controllers
         }
         public IActionResult Index()
         {
-            return View();
+            var branches = _branch.GetAll().Select(branch => new BranchDetailModel
+            {
+                Id = branch.Id,
+                Address = branch.Address,
+                Description = branch.Description,
+                HoursOpen = _branch.GetBranchHours(branch.Id),
+                ImageUrl = branch.ImageUrl,
+                IsOpen = _branch.IsBranchOpen(branch.Id),
+                Name = branch.Name,
+                NumberOfAssets = branch.LibraryAssets.Count(),
+                NumberOfPatrons = branch.Patrons.Count(),
+                OpenDate = branch.OpenDate.ToString("yyyy-MM-dd"),
+                Telephone = branch.Telephone,
+                TotalAssetValue = branch.LibraryAssets.Sum(asset => asset.Cost)
+            });
+
+            var model = new BranchIndexModel
+            {
+                Branches = branches
+            };
+            return View(model);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var branch = _branch.Get(id);
+            var model = new BranchDetailModel
+            {
+                Id = branch.Id,
+                Address = branch.Address,
+                Description = branch.Description,
+                HoursOpen = _branch.GetBranchHours(id),
+                ImageUrl = branch.ImageUrl,
+                IsOpen = _branch.IsBranchOpen(id),
+                Name = branch.Name,
+                NumberOfAssets = branch.LibraryAssets.Count(),
+                NumberOfPatrons = branch.Patrons.Count(),
+                OpenDate = branch.OpenDate.ToString("yyyy-MM-dd"),
+                Telephone = branch.Telephone,
+                TotalAssetValue = branch.LibraryAssets.Sum(asset => asset.Cost)
+            };
+            return View(model);
         }
     }
 }
